eb007118354264cd3447d0cb383a5150e6eed257
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2019-10-07 16:01:19 +0200

    Adjust reference to settings and action constants

--- tests/make_invisible_test.php
+++ tests/make_invisible_test.php
@@ -41,8 +41,8 @@ class lifecyclestep_makeinvisible_make_invisible_testcase extends \advanced_test
         $trigger = $generator->create_trigger('manual', 'manual', $workflow->id);
         $generator->create_step('makeinvisible', 'makeinvisible', $workflow->id);
         $step = $generator->create_step('email', 'email', $workflow->id);
-        settings_manager::save_settings($step->id, SETTINGS_TYPE_STEP, 'email', null);
-        workflow_manager::handle_action(ACTION_WORKFLOW_ACTIVATE, $workflow->id);
+        settings_manager::save_settings($step->id, \tool_lifecycle\settings_type::STEP, 'email', null);
+        workflow_manager::handle_action(\tool_lifecycle\action::WORKFLOW_ACTIVATE, $workflow->id);
 
         // Course1 is visible in an visible category. It should be hidden after step and shown after rollback.
         $course1 = $this->getDataGenerator()->create_course();
